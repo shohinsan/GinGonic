382cf52161a01d959a260bc2c7187abda7e622dc
Shohin Abdulkhamidov <abd.shohin@gmail.com>
2024-04-15 10:41:32 -0700

    feat: update events

--- routes/routes.go
+++ routes/routes.go
@@ -6,4 +6,5 @@ func RegisterRoutes(server *gin.Engine) {
 	server.GET("/events", getEvents)    // GET, POST, PUT, PATCH, DELETE
 	server.GET("/events/:id", getEvent) // /events/1, /events/5
 	server.POST("/events", createEvent)
+	server.PUT("/events/:id", updateEvent)
 }
